f8fc3908244edb8ee093184a6278452e597c5149
Samiya Kazi <samiya.kazi09@gmail.com>
2023-01-18 16:25:12 +0600

    feat: added admin creation for admin route

--- server/controllers/institute.js
+++ server/controllers/institute.js
@@ -1,4 +1,6 @@
 const { Institute } = require("../models/institute");
+const { User } = require("../models/user");
+const bcrypt = require('bcryptjs');
 
 async function getInstitute (req, res) {
   try {
@@ -13,9 +15,22 @@ async function getInstitute (req, res) {
 
 async function postInstitute (req, res) {
   try {
-    const { name, type } = req.body;
-    const result = await Institute.create({name, type})
-    res.status(201).send(result);
+    const { name, type, firstName, lastName, email, password } = req.body;
+    const checkUser = await User.find({email});
+    if (checkUser.length) {
+      res.status(401).send('An account with this email already exists.');
+    } else {
+      const newInstitute = await Institute.create({name, type});
+
+      const salt = bcrypt.genSaltSync();
+      const encryptedPass = bcrypt.hashSync(password, salt);
+      const newAdmin = await User.create({firstName, lastName, email, password: encryptedPass, type: 'admin', institute: newInstitute});
+
+      const projection = {firstName: 1, lastName: 1, email: 1, type: 1, institute: 1};
+      const admin = await User.findById(newAdmin._id, projection);
+      
+      res.status(201).send({institute: newInstitute, admin: admin});
+    }
   } catch (error) {
     console.log(error);
   }
