8fbacd994338858feea4c0de41c84da0ec528213
Samiya Kazi <samiya.kazi09@gmail.com>
2023-01-29 18:50:14 +0600

    fix: store not keeping auth

--- client/src/app/store/reducers/user.reducer.ts
+++ client/src/app/store/reducers/user.reducer.ts
@@ -2,12 +2,14 @@ import { Action, createReducer, on } from '@ngrx/store';
 import { User } from 'src/app/models/user.model';
 import { RemoveUserAction, SetUserAction } from '../actions/user.action';
 
-const initialState: User[] = [];
+const user = localStorage.getItem('user');
+const initialState: User[] = user ? [JSON.parse(user)] : [];
 
 
 const reducer = createReducer(
   initialState,
   on(SetUserAction, (state, action) => {
+    localStorage.setItem('user', JSON.stringify(action.payload));
      return [action.payload];
   }),
   on(RemoveUserAction, (state, action) => {
