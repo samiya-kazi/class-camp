8a90a1b895b4dc1b2217e0b1819e48f044932056
Samiya Kazi <samiya.kazi09@gmail.com>
2023-01-30 10:57:02 +0600

    feat: added basic side nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,12 +11,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './pages/login/login.component';
 import { MatIconModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RegisterComponent } from './pages/register/register.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { UserReducer } from './store/reducers/user.reducer';
+import { BannerComponent } from './components/banner/banner.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,8 @@ import { UserReducer } from './store/reducers/user.reducer';
     RegisterComponent,
     NavBarComponent,
     HomePageComponent,
+    BannerComponent,
+    SideNavComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +42,7 @@ import { UserReducer } from './store/reducers/user.reducer';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
+    MatSidenavModule,
     StoreModule.forRoot({institute: InstituteReducer, user: UserReducer})
   ],
   providers: [],
---
+++ client/src/app/components/banner/banner.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+  imgUrl = 'https://www.cabq.gov/artsculture/biopark/news/10-cool-facts-about-penguins/@@images/1a36b305-412d-405e-a38b-0947ce6709ba.jpeg'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
