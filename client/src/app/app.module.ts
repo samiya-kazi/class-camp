b4d0c155d4741df0c70b8aada7d51a4044252919
Samiya Kazi <samiya.kazi09@gmail.com>
2023-01-31 12:13:44 +0600

    feat: added custom material module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,7 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { InstituteReducer } from './store/reducers/institute.reducer';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './pages/login/login.component';
-import { MatIconModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
 import { RegisterComponent } from './pages/register/register.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -24,6 +18,7 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { AccessTokenInterceptorService } from './services/interceptors/access-token-interceptor/access-token-interceptor.service';
 import { StorageTokenInterceptorService } from './services/interceptors/storage-token-interceptor/storage-token-interceptor.service';
 import { DisplayCardComponent } from './components/display-card/display-card.component';
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -43,12 +38,7 @@ import { DisplayCardComponent } from './components/display-card/display-card.com
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule, 
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatCardModule,
+    MaterialModule,
     StoreModule.forRoot({institute: InstituteReducer, user: UserReducer})
   ],
   providers: [
---
+++ client/src/app/modules/material/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
