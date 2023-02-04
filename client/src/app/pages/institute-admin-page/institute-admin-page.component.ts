aa8c02ea790dde0ae5be8bf72946075b967835ea
Samiya Kazi <samiya.kazi09@gmail.com>
2023-02-04 21:44:23 +0600

    feat: added display cards to admin panel

--- client/src/app/pages/institute-admin-page/institute-admin-page.component.ts
+++ client/src/app/pages/institute-admin-page/institute-admin-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { InstituteClass } from 'src/app/models/class.model';
 import { Institute } from 'src/app/models/institute.model';
 import { State } from 'src/app/models/state.model';
+import { ApiClientService } from 'src/app/services/api-client/api-client.service';
 
 @Component({
   selector: 'app-institute-admin-page',
@@ -11,12 +13,21 @@ import { State } from 'src/app/models/state.model';
 export class InstituteAdminPageComponent implements OnInit {
 
   institute!: Institute;
+  classes : InstituteClass[] = [];
 
-  constructor(private store: Store<State>) { }
+  constructor(
+    private store: Store<State>,
+    private api: ApiClientService) { }
 
   ngOnInit(): void {
     const institute$ = this.store.select(store => store.institute);
     institute$.subscribe(institute => this.institute = institute);
+    this.getClasses();
+  }
+
+  getClasses () {
+    if (this.institute._id)
+      this.api.getClasses(this.institute._id).subscribe(classes => this.classes = classes)
   }
 
 }
