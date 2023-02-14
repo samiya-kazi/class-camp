3ce58a2eb82471e5d6991cc3744348627ea5aaa8
Samiya Kazi <samiya.kazi09@gmail.com>
2023-02-14 12:17:19 +0600

    feat: added assignment model to server

---
+++ server/models/assignment.js
@@ -0,0 +1,35 @@
+const { model, Schema } = require('mongoose');
+const { userSchema } = require('./user');
+
+const assignmentSchema = new Schema({
+  classId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: Text,
+    required: true
+  },
+  totalMarks: {
+    type: Number,
+    required: true 
+  },
+  postedDate: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
+  dueDate: Date,
+  postedBy: {
+    type: userSchema,
+    required: true
+  },
+});
+
+const Assignment = model('Assignment', assignmentSchema);
+
+module.exports = { assignmentSchema, Assignment }
